0c6b113985293207736012a986607c386aefc65f
IsaaQQ <opeyemifashola6@gmail.com>
2024-07-18 00:22:43 -0700

    new changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom"
 
 const Banner = () => {
   return (
-    <div className='banner d-flex justify-content-center justify-content-md-start align-items-center px-3'>
-        <div className= "text-start">
+    <div 
+    className='banner d-flex justify-content-center justify-content-md-start align-items-center px-3'>
+        <div 
+        className= "text-start">
             <h1 className = "text-success display-5 fw-bold">Healthy Foods</h1>
             <h1 className ="fw-light">Explore different meals</h1>
             <button className="btn btn-success">
-                <a href= "#below" className="text-decoration-none text-white">See More Below</a>
+                <a href= "#below" className="text-decoration-none text-white fw-light">See More Below</a>
             </button>
         </div>
     </div>
